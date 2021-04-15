4d8aac45da63a7e2a38b55753dcbeb4c01ec1eee
chrislemus <icrislemus@gmail.com>
2021-04-15 17:54:04 -0400

    commit tree be55c31ee3f275c922d10ea977de1238b9c71546

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import BandInput from './BandInput';
-
-import { connect } from 'react-redux'
+import Band from './Band';
+import { connect } from 'react-redux';
 
 class BandsContainer extends Component {
+  renderBands = () =>
+    this.props.bands.map((band, idx) => (
+      <Band key={idx + band} band={band} deleteBand={this.props.deleteBand} />
+    ));
   render() {
+    console.log(this.props.bands);
     return (
       <div>
-        <BandInput addBand={this.props.addBand}/>
-
+        <BandInput addBand={this.props.addBand} />
+        {this.renderBands()}
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = ({ bands }) => ({ bands })
+const mapStateToProps = ({ bands }) => ({ bands });
 
-const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
-})
+const mapDispatchToProps = (dispatch) => ({
+  addBand: (name) => dispatch({ type: 'ADD_BAND', name }),
+  deleteBand: (id) => dispatch({ type: 'DELETE_BAND', id }),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,12 +1,20 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
+export default function manageBand(
+  state = {
+    bands: [],
+  },
+  action
+) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const id = state.bands.length + action.name;
+      return {
+        ...state,
+        bands: [...state.bands, { name: action.name, id: id }],
+      };
+    case 'DELETE_BAND':
+      console.log(action);
+      return { bands: state.bands.filter((band) => band.id != action.id) };
     default:
       return state;
   }
-};
+}
